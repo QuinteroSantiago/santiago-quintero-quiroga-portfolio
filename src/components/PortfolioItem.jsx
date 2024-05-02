31b36414ab8c617dae4e285c26e02bd1e62765bf
Santiago Quintero <santiago@corellium.com>
2024-05-02 17:40:58 -0400

    Fix React issues

--- src/components/PortfolioItem.jsx
+++ src/components/PortfolioItem.jsx
@@ -23,8 +23,8 @@ function PortfolioItem({ title, imgUrl, stack, link, date }) {
          <div className="w-full p-2">
             <h3 className="text-lg md:text-xl dark:text-black mb-2 md:mb-3 font-semibold">{title}</h3>
             <p className="flex flex-wrap gap-2 flex-row items-center justify-start text-xs md:text-sm dark:text-black ">
-               {stack.map(item => (
-                  <span className="inline-block px-2 py-1 border border-stone-900 rounded-md">
+               {stack.map((item, index) => (
+                  <span key={index} className="inline-block px-2 py-1 border border-stone-900 rounded-md">
                      {item}
                   </span>
                ))}
