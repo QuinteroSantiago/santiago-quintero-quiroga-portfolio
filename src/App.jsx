30d0f700be0c744ea204276bca02a5d385ecbf30
Santiago Quintero <santiago@gomu.cloud>
2024-07-08 23:40:54 -0400

    [minor]: Add blog posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
+import Blog from './pages/Blog'; 
 
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			<BrowserRouter>
 				<Routes>
 					<Route index element={<Home />} />
+					<Route path="blog" element={<Blog />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
