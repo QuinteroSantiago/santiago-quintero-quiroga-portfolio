a869c1139cf03ac51a29d8ee9ca79305e591b027
Santiago Quintero <santiago@corellium.com>
2024-07-12 11:32:48 -0400

    chore: Add blog entry

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { BrowserRouter, Routes, Route, Link, useLocation } from 'react-router-do
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import Home from './pages/Home'
-import Blog from './pages/Blog'; 
+import Blog from './pages/Blog';
 import NotFound from './pages/404'
 
 function App() {
     return (
         <BrowserRouter>
             <div className='dark:bg-zinc-900 text-zinc-900 dark:text-zinc-300'>
-				<Navigation />
+                <Navigation />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/blog" element={<Blog />} />
