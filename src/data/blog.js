c5bfb355e1ccc84ff5f93c6660f293bef1756063
Santiago Quintero <santiago@corellium.com>
2024-07-12 11:32:48 -0400

    chore: Add blog entry

--- src/data/blog.js
+++ src/data/blog.js
@@ -1,11 +1,28 @@
 export default [
     {
+        id: 4,
+        title: 'Fawcous',
+        date: '20:31, Thursday, 07-11-2024',
+        content: `
+            Today is 711. Worked from the moment I awoke until now. But twas okay, very productive day. Just have a couple of things to wrap up for tomorrow thankfully and I should be done. I like finishing stuff.
+            We are still watching GoT, season 4 is very good, loli was surprised by who killed Joffrey, she didn't expect that.
+            I am feeling content, but have to code more, create more software, I'm finally starting to enjoy it throughly, time passes by flying when solving issues.
+            Didn't go to the f*cking gym... Tomorrow's double day, I am going to read this tomorrow morning as well, it's in my mental todo list.
+            Finally wrote something new today:
+            >>poem
+            Enduring
+            Momentum building,
+            Keeping your eternal vows,
+            No current of tears
+            Miami, FL, USA (07.11.24)<<`
+    },
+    {
         id: 3,
         title: 'Lethargic kind of day',
         date: '22:32, Wednesday, 07-10-2024',
         content: `
             Woke up at 0500 to charge the car for the lady. A lot of moving parts, lots of releases, lots of lines of code to look at, it was more of a merging and fixing day than building unfortunately. Not too bad though, fixed most things, some things are still broken. We finally saw the bastard Joffrey die, he finally had won as well, nice.
-            As for yesterday, Spain won and I watched the game at juliet and henry's, or at least the second half. Twas fun, should do again. And I did end up working out, at least the jog to the place and back killed me. Didn't reaallyy watch either semifinal, these are both the losing teams for Sunday either way. Need to workout tomorrow, can't miss that since I didn't today.
+            As for yesterday, Spain won and I watched the game at juliet and henry's, or at least the second half. Twas fun, should do again. And I did end up working out, at least the jog to the place and back killed me. Didn't reaallyy watch either semifinal, these are both the losing teams for Sunday either way. Need to workout tomorrow, can't miss that since I didn't go today.
             Also, writing this late at night is no good for me, have too many thoughts scrambled here and there. Finished a deployment now so I can rest.
             My poems are also now clearly deteriorating as I'm running out of old material, will leave you with this 'masterpiece' I tried to write from my vast knowledge of japanese.
             >>poem
@@ -20,9 +37,9 @@ export default [
         title: 'Plan plan plan',
         date: '08:10, Tuesday, 07-09-2024',
         content: `
-            Today will be cloudy all day.\n
-            Will try to watch the Spain vs France game, work willing. Odds are slightly in favor of Spain (+170 vs +200), I predict a 1-0 win for them.\n
-            Want to use my Gomu Planner app a bit more today to plan. I will have to review the logs at night to get the full benefits as well. I am writing this down so I will work out today, at least at my home gym. We'll see what happens where when the time comes.\n
+            Today will be cloudy all day.
+            Will try to watch the Spain vs France game, work willing. Odds are slightly in favor of Spain (+170 vs +200), I predict a 1-0 win for them.
+            Want to use my Gomu Planner app a bit more today to plan. I will have to review the logs at night to get the full benefits as well. I am writing this down so I will work out today, at least at my home gym. We'll see what happens where when the time comes.
             I have another poem I wrote on that trip to the Bahamas; it will be hard to understand if you weren't there though.
             >>poem
             Da River
@@ -41,9 +58,9 @@ export default [
             I would like to journal daily, we will see how that goes. Blah blah blah blah, need to fill this up a bit, I will leave you with a poem I wrote a bit ago:
             >>poem
             Da Grey Beneath
-            Blue black infinity,\n
-            Shadows in your trenches,\n
-            humanity's greatest fear,\n
+            Blue black infinity,
+            Shadows in your trenches,
+            humanity's greatest fear,
             a trove of friendship
             Chub Cay, BS (06.22.24)<<`
     },
