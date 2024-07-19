f215f329d8eb5dabcaa601cad65745196100a589
Santiago Quintero <santiago@gomu.cloud>
2024-07-19 18:43:26 -0400

    [minor]: 3js rotating image

--- src/components/Home/Intro.jsx
+++ src/components/Home/Intro.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import RotatingImage from './RotatingImage';
 
 function Intro() {
    const startDate = new Date('2021-05-01');
@@ -10,11 +11,7 @@ function Intro() {
    const roundedYears = fractionalYear >= 0.5 ? diffYears + 1 : diffYears;
    return (
       <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
-         <img
-            src="/assets/IMG_2077.jpg"
-            alt="portfolio"
-            className="rounded-full w-48 h-48 object-cover cursor-pointer border border-black dark:border-white"
-         />
+         <RotatingImage />
          <h1 className="text-4xl dark:text-white mb-1 md:mb-3">Santiago Quintero</h1>
          <p className="text-base md:text-xl mb-3 font-medium">Software Engineer</p>
          <p className="text-md max-w-xl mb-6">
