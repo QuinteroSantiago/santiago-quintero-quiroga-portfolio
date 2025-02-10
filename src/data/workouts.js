a736dd36a19147c84b8896fad6a301168b2b457e
Santiago Quintero <santiago@corellium.com>
2025-02-10 12:14:30 -0500

    chore: update to 4 days a week workout

--- src/data/workouts.js
+++ src/data/workouts.js
@@ -10,34 +10,34 @@ export default {
                 weight: 'Body weight'
             },
             {
-                name: 'Barbell Rows',
+                name: 'Lat Pulldowns',
                 sets: '4',
                 reps: '10 reps',
-                weight: '135'
+                weight: '150'
             },
             {
                 name: 'Seated Row',
-                sets: '3',
+                sets: '4',
                 reps: '10 reps',
                 weight: '80'
             },
             {
-                name: 'EZ Bar Standing Curl',
-                sets: '3',
-                reps: '12 reps',
+                name: 'Machine Bicep Curl',
+                sets: '4',
+                reps: '10 reps',
                 weight: '50'
             },
             {
-                name: 'Machine Bicep Curl',
-                sets: '3',
-                reps: '10 reps',
-                weight: '50'
-            }
+                name: 'DB Curls',
+                sets: '4',
+                reps: '12 reps',
+                weight: '25'
+            },
         ]
     },
     'Tuesday': {
         title: 'Leg Day',
-        time: '60 mins',
+        time: '90 mins',
         exercises: [
             {
                 name: 'Back Squats',
@@ -47,7 +47,7 @@ export default {
             },
             {
                 name: 'Leg Press',
-                sets: '3',
+                sets: '4',
                 reps: '10 reps',
                 weight: '320'
             },
@@ -72,42 +72,6 @@ export default {
         ]
     },
     'Wednesday': {
-        title: 'Upper Body',
-        time: '60 mins',
-        exercises: [
-            {
-                name: 'Pec Deck',
-                sets: '4',
-                reps: '10 reps',
-                weight: '130'
-            },
-            {
-                name: 'Lat Pulldowns',
-                sets: '4',
-                reps: '10 reps',
-                weight: '150'
-            },
-            {
-                name: 'Cable Flyes',
-                sets: '4',
-                reps: '12 reps',
-                weight: '50'
-            },
-            {
-                name: 'Cable Curls',
-                sets: '3',
-                reps: '12 reps',
-                weight: '25'
-            },
-            {
-                name: 'Cable Extension',
-                sets: '4',
-                reps: '12 reps',
-                weight: '40'
-            },
-        ]
-    },
-    'Thursday': {
         title: 'Cardio',
         time: '30 mins',
         exercises: [
@@ -131,7 +95,67 @@ export default {
             }
         ]
     },
+    'Thursday': {
+        title: 'Upper Body',
+        time: '60 mins',
+        exercises: [
+            {
+                name: 'Pec Deck',
+                sets: '4',
+                reps: '10 reps',
+                weight: '130'
+            },
+            {
+                name: 'Cable Flyes',
+                sets: '4',
+                reps: '12 reps',
+                weight: '50'
+            },
+            {
+                name: 'Cable Curls',
+                sets: '3',
+                reps: '12 reps',
+                weight: '25'
+            },
+            {
+                name: 'Tricep Cable Extension',
+                sets: '4',
+                reps: '12 reps',
+                weight: '40'
+            },
+            {
+                name: 'Incline DB Curls',
+                sets: '3',
+                reps: '10-12 reps',
+                weight: '20'
+            }
+        ]
+    },
     'Friday': {
+        title: 'Cardio',
+        time: '40 mins',
+        exercises: [
+            {
+                name: 'Incline Treadmill Walk',
+                sets: '20 minutes',
+                reps: '3 mph',
+                weight: '15% incline'
+            },
+            {
+                name: 'Hanging Leg Raises',
+                sets: '4',
+                reps: '12 reps',
+                weight: '0'
+            },
+            {
+                name: 'Weighted Crunches',
+                sets: '4',
+                reps: '12 reps',
+                weight: '20'
+            }
+        ]
+    },
+    'Saturday': {
         title: 'Push Day',
         time: '60 mins',
         exercises: [
@@ -154,6 +178,12 @@ export default {
                 weight: '5'
             },
             {
+                name: 'Face Pulls',
+                sets: '4',
+                reps: '12 reps',
+                weight: '5'
+            },
+            {
                 name: 'Tricep Cable Kickback',
                 sets: '3',
                 reps: '12 reps',
@@ -161,44 +191,9 @@ export default {
             }
         ]
     },
-    'Saturday': {
-        title: 'Leg & Arms',
-        time: '60 mins',
-        exercises: [
-            {
-                name: 'Front Squats',
-                sets: '4',
-                reps: '8-10 reps',
-                weight: '135'
-            },
-            {
-                name: 'Walking Lunges',
-                sets: '4',
-                reps: '12-15 reps per leg',
-                weight: '25'
-            },
-            {
-                name: 'Standing Calf Raises',
-                sets: '4',
-                reps: '12-15 reps',
-                weight: ''
-            },
-            {
-                name: 'Incline DB Curls',
-                sets: '3',
-                reps: '10-12 reps',
-                weight: '20'
-            },
-            {
-                name: 'Tricep Cable Extension',
-                sets: '4',
-                reps: '12 reps',
-                weight: '20'
-            }
-        ]
-    },
     'Sunday': {
         title: 'Cardio',
+        time: '40 mins',
         exercises: [
             {
                 name: 'Incline Treadmill Walk',
