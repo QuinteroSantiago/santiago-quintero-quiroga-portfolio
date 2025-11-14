66ad17d402641398429432c287532753040f5a99
Santiago Quintero <santiago@gomu.cloud>
2025-11-14 12:23:32 -0500

    build: update workout for strength and endurance

---
+++ src/data/archive/workouts4.js
@@ -0,0 +1,218 @@
+export default {
+    'Monday': {
+        title: 'Pull Day',
+        time: '75 mins',
+        exercises: [
+            {
+                name: 'Warmup',
+                sets: '',
+                reps: '10 minutes',
+                weight: ''
+            },
+            {
+                name: 'Pull-Ups',
+                sets: '4',
+                reps: '6 reps',
+                weight: 'Body weight'
+            },
+            {
+                name: 'DB Row (Individual Arm)',
+                sets: '4',
+                reps: '10 reps',
+                weight: '45'
+            },
+            {
+                name: 'BB Curls',
+                sets: '4',
+                reps: '10 reps',
+                weight: '50'
+            },
+            {
+                name: 'Lat Pulldowns',
+                sets: '4',
+                reps: '10 reps',
+                weight: '140'
+            },
+            {
+                name: 'Machine Bicep Curl',
+                sets: '4',
+                reps: '10 reps',
+                weight: '40'
+            },
+        ]
+    },
+    'Tuesday': {
+        title: 'Leg Day',
+        time: '90 mins',
+        exercises: [
+            {
+                name: 'Warmup',
+                sets: '',
+                reps: '10 minutes',
+                weight: ''
+            },
+            {
+                name: 'Back Squats',
+                sets: '4',
+                reps: '6 reps',
+                weight: '225'
+            },
+            {
+                name: 'Leg Press',
+                sets: '4',
+                reps: '10 reps',
+                weight: '320'
+            },
+            {
+                name: 'Leg Curls',
+                sets: '4',
+                reps: '12 reps',
+                weight: '110'
+            },
+            {
+                name: 'Leg Extension',
+                sets: '4',
+                reps: '12 reps',
+                weight: '120'
+            },
+            {
+                name: 'Calf Raises',
+                sets: '4',
+                reps: '15 reps',
+                weight: ''
+            }
+        ]
+    },
+    'Wednesday': {
+        title: 'Cardio (HIIT)',
+        time: '40 mins',
+        exercises: [
+            {
+                name: 'Rowing Machine HIIT https://www.rowingmachineworkouts.com/rowing-workouts/hiit-rowing',
+                sets: '3 rounds',
+                reps: '(30s sprint / 30s easy) x 3 + 2 minute rest',
+                weight: ''
+            },
+            {
+                name: 'Weighted Crunches',
+                sets: '4',
+                reps: '12 reps',
+                weight: '80'
+            },
+            {
+                name: 'Hanging Leg Raises',
+                sets: '4',
+                reps: '6 reps',
+                weight: '0'
+            }
+        ]
+    },
+    'Thursday': {
+        title: 'Upper Body',
+        time: '60 mins',
+        exercises: [
+            {
+                name: 'Pec Deck',
+                sets: '4',
+                reps: '10 reps',
+                weight: '160'
+            },
+            {
+                name: 'Cable Flyes',
+                sets: '4',
+                reps: '12 reps',
+                weight: '50'
+            },
+            {
+                name: 'Cable Curls',
+                sets: '3',
+                reps: '12 reps',
+                weight: '25'
+            },
+            {
+                name: 'Tricep Cable Extension',
+                sets: '4',
+                reps: '12 reps',
+                weight: '40'
+            },
+            {
+                name: 'Hammer Curls',
+                sets: '3',
+                reps: '10 reps',
+                weight: '20'
+            }
+        ]
+    },
+    'Friday': {
+        title: 'Cardio',
+        time: '40 mins',
+        exercises: [
+            {
+                name: 'Incline Treadmill Walk',
+                sets: '15% incline',
+                reps: '40 minutes',
+                weight: '3 mph'
+            }
+        ]
+    },
+    'Saturday': {
+        title: 'Push Day',
+        time: '60 mins',
+        exercises: [
+            {
+                name: 'Bench Press',
+                sets: '4',
+                reps: '8 reps',
+                weight: '185'
+            },
+            {
+                name: 'Incline DB Press',
+                sets: '4',
+                reps: '10 reps',
+                weight: '90'
+            },
+            {
+                name: 'Cable Lateral Raises',
+                sets: '4',
+                reps: '12 reps',
+                weight: '5'
+            },
+            {
+                name: 'Face Pulls',
+                sets: '4',
+                reps: '12 reps',
+                weight: '5'
+            },
+            {
+                name: 'Tricep Cable Kickback',
+                sets: '3',
+                reps: '12 reps',
+                weight: '5'
+            }
+        ]
+    },
+    'Sunday': {
+        title: 'Cardio',
+        time: '40 mins',
+        exercises: [
+            {
+                name: 'Bike HIIT',
+                sets: '6 rounds',
+                reps: '45s hard / 2 min easy',
+                weight: ''
+            },
+            {
+                name: 'Weighted Crunches',
+                sets: '4',
+                reps: '12 reps',
+                weight: '80'
+            },
+            {
+                name: 'Hanging Leg Raises',
+                sets: '4',
+                reps: '6 reps',
+                weight: '0'
+            }
+        ]
+    }
+};
